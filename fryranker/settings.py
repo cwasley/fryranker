04814782df522d4cb0f714a861b3c8ca5a8b7b58
Collin Wasley <collin.wasley@gmail.com>
2020-03-01 17:15:44 -0800

    Updated settings.

--- fryranker/settings.py
+++ fryranker/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'fryranker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fryranked',
+        'NAME': 'fryranker',
         'USER': 'admin',
         'PASSWORD': '',
         'HOST': 'localhost',
